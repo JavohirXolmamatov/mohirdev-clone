4581562187cf12391fcc52d2009fe8c02df970f1
JavohirXolmamatov <xolmamatovjavohir2002@gmail.com>
2024-08-15 10:40:36 +0500

    add extras

--- Js.js
+++ Js.js
@@ -11,3 +11,39 @@ document.getElementById('scroll-right').addEventListener('click', function() {
         behavior: 'smooth'
     });
 });
+
+
+document.getElementById('show-more').addEventListener('click', function() {
+    var hiddenRows = document.querySelectorAll('.hidden-row');
+    hiddenRows.forEach(function(row) {
+        row.style.display = 'block';
+    });
+    this.style.display = 'none'; // Knopkani yashirish
+});
+
+document.getElementById('show-more2').addEventListener('click', function() {
+    var hiddenRows = document.querySelectorAll('.hidden-row2');
+    hiddenRows.forEach(function(row) {
+        row.style.display = 'block';
+    });
+    
+    // Ko'proq ko'rsatish knopkasini yashirish
+    this.style.display = 'none'; 
+    
+    // Collarni qisqartirish knopkasini ko'rsatish
+    document.getElementById('show-less2').style.display = 'inline-block'; 
+});
+
+document.getElementById('show-less2').addEventListener('click', function() {
+    var hiddenRows = document.querySelectorAll('.hidden-row2');
+    hiddenRows.forEach(function(row) {
+        row.style.display = 'none';
+    });
+    
+    // Collarni qisqartirish knopkasini yashirish
+    this.style.display = 'none'; 
+    
+    // Ko'proq ko'rsatish knopkasini ko'rsatish
+    document.getElementById('show-more2').style.display = 'inline-block'; 
+});
+
